64aba32f08e17e349d81a8d3dae7753e03545020
wt <wt@163.com>
2016-08-01 10:26:37 +0800

    进度条

---
+++ app/src/main/java/com/a/handletraining/MainActivity.java
@@ -0,0 +1,90 @@
+package com.a.handletraining;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private ImageView mImageView;
+    private Button mLoadImageButton;
+    private Button mToastButton;
+    private ProgressBar mProgressBar;
+    private Handler mHandler = new Handler(Looper.getMainLooper()) {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {//根据收到的消息类型处理
+                case 0:
+                    mProgressBar.setVisibility(View.VISIBLE);
+                    break;
+                case 1:
+                    mProgressBar.setProgress((int) msg.obj);
+                    break;
+                case 2:
+                    mImageView.setImageBitmap((Bitmap) msg.obj);
+                    break;
+            }
+        }
+    };
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mImageView = (ImageView) findViewById(R.id.activity_main_image_view);
+        mLoadImageButton = (Button) findViewById(R.id.activity_main_load_image_button);
+        mToastButton = (Button) findViewById(R.id.activity_main_toast_button);
+        mProgressBar = (ProgressBar) findViewById(R.id.activity_main_progress_bar);
+        mLoadImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Thread(
+                        new Runnable() {
+                            @Override
+                            public void run() {
+                                Message msg = mHandler.obtainMessage();
+                                msg.what = 0; //表示让progressbar显示出来
+                                mHandler.sendMessage(msg);
+                                for (int i = 1; i < 11; i++) {
+                                    sleep();
+                                    Message msg2 = mHandler.obtainMessage();
+                                    msg2.what = 1; //表示让progressbar往前走
+                                    msg2.obj = i * 10;
+                                    mHandler.sendMessage(msg2);
+                                }
+                                Bitmap bitmap= BitmapFactory.decodeResource(getResources(),R.drawable.ic_launcher);
+                                Message bitMsg=mHandler.obtainMessage();
+                                bitMsg.what=2;
+                                bitMsg.obj=bitmap;
+                                mHandler.sendMessage(bitMsg);
+                            }
+
+                            private void sleep() {
+                                try {
+                                    Thread.sleep(500);
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }
+                ).start();
+            }
+        });
+        mToastButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "xxxxxxxxxxxxx", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
